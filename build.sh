6023bdf59c103cf46ab051a5297874b60e51bab7
George Chan <gchan9527@gmail.com>
2025-09-04 22:49:34 +0800

    update build script

--- build.sh
+++ build.sh
@@ -4,16 +4,21 @@ find -name "*.so" -exec rm {} \;
 find -name "*.a" -exec rm {} \;
 find -name "libgallium_dri.*" -exec rm -r {} \;
 
-	meson --reconfigure "build-android" \
+
+# platform_sdk >= 30 enable gralloc4
+#              >= 35 enable libui
+
+	meson setup --reconfigure "build-android" \
             --prefix=/tmp/mesa \
             --cross-file "../build-crossfile" \
             -Dplatforms=android \
-            -Dplatform-sdk-version=34 \
+            -Dplatform-sdk-version=35 \
             -Dandroid-stub=true \
             -Dandroid-libbacktrace=disabled \
             -Dandroid-strict=true \
             -Dxlib-lease=disabled \
             -Degl=true \
+	    -Degl-native-platform=android \
             -Dgbm=false \
             -Dglx=disabled \
             -Dllvm=false \
@@ -22,8 +27,8 @@ find -name "libgallium_dri.*" -exec rm -r {} \;
             -Dgles2=true \
             -Dvulkan-drivers=freedreno \
             -Dtools=drm-shim \
-            -Dgallium-drivers=zink,freedreno \
-            -Dfreedreno-kmds=kgsl,msm \
+            -Dgallium-drivers=freedreno \
+            -Dfreedreno-kmds=kgsl \
 	    -Dallow-fallback-for=libdrm \
             -Dbuildtype=release \
             --force-fallback-for=libdrm,libxml2 \
@@ -31,4 +36,5 @@ find -name "libgallium_dri.*" -exec rm -r {} \;
             -Dlibdrm:freedreno=enabled \
             -Dlibdrm:default_library=static \
 
+#	meson configure build-android/ 
           ninja -C "build-android" install
